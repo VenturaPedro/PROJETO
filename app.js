667d63ac21793b10d610be93d64163e88b744be3
VenturaPedro <pedroventuramoura@gmail.com>
2023-11-10 00:49:02 -0300

    #

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const mysql = require('mysql2');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
+//conexão com o banco
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
@@ -12,6 +14,8 @@ const db = mysql.createConnection({
   database: 'projeto'
 });
 
+
+//mensagem de conexão
 db.connect(err => {
   if (err) {
     console.error('Erro na conexão com o banco de dados:', err);
@@ -20,21 +24,44 @@ db.connect(err => {
   console.log('Conexão com o banco de dados estabelecida');
 });
 
+
+//ROTAS DE GET
+
 app.get("/", (req, res) => {
-  res.send("Pagina Inicial");
+    res.sendFile(__dirname + "/frontend/telaPrincipal.html");
 });
 
-app.get("/cadastrar", (req, res) => {
+app.get("/LOGIN", (req, res) => {
+    res.sendFile(__dirname + "/frontend/LOGIN.html");
+});
+
+app.get("/caixa", (req, res) => {
+    res.sendFile(__dirname + "/frontend/statusCaixa.html");
+});
+
+app.get("/painel",(req, res) => {
+    res.sendFile(__dirname + "/frontend/painel.html");
+});
+
+app.get("/cadastrar",(req, res) => {
   res.sendFile(__dirname + "/frontend/cadastros.html");
 });
 
-app.use(express.static('public'))
+
+//-------------------------------------------------------------------
+app.use(express.static('frontend'))
+
+
+//ROTAS POST
 
 app.post("/processar-cadastro-cliente", (req, res) => {
-  const { nomeCliente, emailCliente, cpfCliente, telefoneCliente } = req.body;
+  const { nomeCliente, emailCliente, cpfCliente, telefoneCliente, 
+        logradouroCliente, cidadeCliente, complementoCliente, numeroCliente, 
+        estadoCliente, cepCliente } = req.body;
 
-  const sql = 'INSERT INTO clientes (nome, email, cpf, telefone) VALUES (?, ?, ?, ?)';
-  db.query(sql, [ nomeCliente, emailCliente, cpfCliente, telefoneCliente], (err, result) => {
+  const sql = 'INSERT INTO clientes (nome, email, cpf, telefone, logradouro, cidade, complemento, numero, estado, cep) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
+  db.query(sql, [ nomeCliente, emailCliente, cpfCliente, telefoneCliente, logradouroCliente, cidadeCliente, complementoCliente, numeroCliente, 
+    estadoCliente, cepCliente], (err, result) => {
     if (err) {
       console.error('Erro ao inserir dados:', err);
       res.send('Erro ao cadastrar dados no banco de dados');
@@ -45,6 +72,8 @@ app.post("/processar-cadastro-cliente", (req, res) => {
   });
 });
 
+
+//MENSAGEM STATUS CONEXÃO
 app.listen(8080, () => {
   console.log("Servidor Iniciado na porta 8080: http://localhost:8080");
 });
